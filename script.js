600325cd26da336d711982c790c2d5287f51545b
nguyen vinh pha <pha.nv@neo-lab.vn>
2019-12-02 14:20:02 +0700

    add search - not done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	  return this.each(function(index){
 	  	let value = null;
 	    let jsonData = xmlToJson(this);
+	    console.log(jsonData);
 	    let currentPosition = [];
 	    let oldPosition = [];
 	    let currentTime = (new Date()).getTime() + index;
@@ -14,10 +15,12 @@ $(document).ready(function(){
 	    	+ "</div>"
 	    $(this).after(html);
 
+	    if(config.search) $('.select-submenu-'+currentTime).find('.content-option').addClass('has_search');
+
 	    $('.select-submenu-'+currentTime).on('click', '.button', function(){
 	    	if(!$('.select-submenu-'+currentTime).find('.content-option').hasClass('show')) {
 		    	currentPosition = oldPosition.slice();
-		    	let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value);
+		    	let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value, config);
 		    	$(this).parent().find('.content-option').html(contentHtml);
 		    	$('.select-submenu-'+currentTime).find('.content-option').addClass('show');
 		    }
@@ -27,7 +30,7 @@ $(document).ready(function(){
 	    		currentPosition.push($(this).data("position"));
 
 	    		$('.select-submenu-'+currentTime).find('.content-option').removeClass('show');
-	    		let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value);
+	    		let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value, config);
 	    		$(this).parent().parent().html(contentHtml);
 	    		setTimeout(function(){
 	    			$('.select-submenu-'+currentTime).find('.content-option').addClass('show');
@@ -44,11 +47,24 @@ $(document).ready(function(){
 	    $('.select-submenu-'+currentTime).on('click', '.back', function(){
 	    	currentPosition.splice(currentPosition.length - 1, 1);
 	    	$('.select-submenu-'+currentTime).find('.content-option').removeClass('show');
-	    	let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value);
+	    	let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value, config);
 	    	$(this).parent().parent().html(contentHtml);
 	    	setTimeout(function(){
 	    		$('.select-submenu-'+currentTime).find('.content-option').addClass('show');
 	    	});
+	    });
+	    $('.select-submenu-'+currentTime).on('keyup', '.search-input', function(){
+	    	let textSearch = $('.select-submenu-'+currentTime+' .search-input').eq(0).val();
+	    	console.log('searching', textSearch);
+	    	let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value, config);
+	    	if(textSearch.length > 0) {
+	    		let htmlSearch = generateListSearchToHtml(searchInJson(jsonData, textSearch));
+		    	$('.select-submenu-'+currentTime).find('.list-option').remove();
+			    $('.select-submenu-'+currentTime).find('.list-search-container').html(htmlSearch);
+			} else {
+				let contentHtml = generateHtmlContentOption(jsonData, currentPosition, value, config);
+				$('.select-submenu-'+currentTime).find('.content-option').html(contentHtml);
+			}
 	    });
 	    let currentHover = false;
 	    $('.select-submenu-'+currentTime).hover(function(){
@@ -72,7 +88,7 @@ function generateButton(config, textLable) {
 	return html;
 }
 
-function generateHtmlContentOption(jsonData, position, value = null) {
+function generateHtmlContentOption(jsonData, position, value = null, config = {}, textSearch = '') {
 	let data = null;
 	data = jsonData;
 	let parentLabel = null;
@@ -85,7 +101,15 @@ function generateHtmlContentOption(jsonData, position, value = null) {
 	} else {
 		data = jsonData.children;
 	}
-	let html = "<ul>";
+	let html = '';
+	if (config.search) {
+		html += "<div class='search-container'>";
+		html += "<ul class='search-group'><li><input style='min-width: 120px;' placeholder='search' type='text' class='search-input form-control' id='pwd'></li></ul>";
+		html += "<div class='list-search-container'></div>"
+		html += "</div>";
+	}
+	console.log(textSearch);
+	html += "<ul class='list-option'>";
 	if (parentLabel) {
 		html += "<li class='parent-label'><button class='btn btn-link btn-xs back'><span class='glyphicon'>&#xe079;</span></button>"+parentLabel+"</li>";
 	}
@@ -97,6 +121,40 @@ function generateHtmlContentOption(jsonData, position, value = null) {
 		}
 	}
 	html += "</ul>";
+	return html;
+}
+
+function searchInJson(jsonData, stringSearch = '', recall = false) {
+	let data = jsonData.children;
+	let dataSearch = [];
+	for(let i in data) {
+		if(data[i].children) {
+			dataSearch.push({
+				name: data[i].label,
+				children: searchInJson(data[i], stringSearch)
+			});
+		} else {
+			if(data[i].content.search(stringSearch) !== -1) {
+				dataSearch.push(data[i]);
+			}
+		}
+	}
+	return dataSearch;
+}
+
+function generateListSearchToHtml(listSearch) {
+	let html = "";
+	html += "<ul class='list-search'>";
+	for(let i in listSearch) {
+		if(typeof listSearch[i].children !== 'undefined' && listSearch[i].children.length > 0) {
+			html += "<li class='list-search-parent-label'>"+listSearch[i].name+"</li>";
+			html += generateListSearchToHtml(listSearch[i].children);
+		} else if(typeof listSearch[i].content !== 'undefined' && listSearch[i].content !== null && listSearch[i].content.length > 0) {
+			html += "<li class='search-item'>"+listSearch[i].content+"</li>";
+		}
+	}
+	html += "</ul>";
+	console.log(html);
 	return html;
 }
 
